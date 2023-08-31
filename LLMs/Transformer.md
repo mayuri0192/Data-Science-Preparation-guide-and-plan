632fa369bd44732e404f90a6e954ed57a84e2eaa
mayuri0192 <mvd.mayuri@gmail.com>
2023-08-31 16:16:16 -0700

    added new project for language translation use case

--- LLMs/Transformer.md
+++ LLMs/Transformer.md
@@ -1,5 +1,14 @@
-<!-- start writing here -->
-<!-- test -->
-<!-- test2 -->
-<!-- test for signed commit  -->
-<!-- test 2 -->
+Transformers vs LSTMs: 
+
+
+| Aspect                     | Transformers                                 | LSTMs (Long Short-Term Memory)               |
+|----------------------------|----------------------------------------------|---------------------------------------------|
+| Parallel Processing        | Process entire sequences in parallel         | Process sequences sequentially             |
+| Attention Mechanism        | Self-attention captures long-range dependencies | Limited context window                    |
+| Positional Encodings       | Use positional encodings to indicate order   | Inherently understand order                |
+| Hardware Acceleration      | Highly parallelizable for hardware acceleration | Limited parallel processing               |
+| Scalability                | Efficient with large datasets                | May struggle with very long sequences      |
+| Gradient Propagation       | More stable gradient propagation for deep models | Vanishing gradient problem in deep networks |
+| Bidirectional Context      | Bidirectional attention for better context   | Unidirectional context for each token       |
+| Hyperparameters           | Fewer hyperparameters to tune                | More hyperparameters for tuning            |
+| Applicability             | Highly effective for NLP tasks                | Widely used for various sequence tasks     |
---
+++ Pytorch/FashionMnist_prediction.py
@@ -0,0 +1,168 @@
+
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Download training data from open datasets.
+training_data = datasets.FashionMNIST(
+    root="Pytorch/data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="Pytorch/data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+######################################################################
+# We pass the ``Dataset`` as an argument to ``DataLoader``. This wraps an iterable over our dataset, and supports
+# automatic batching, sampling, shuffling and multiprocess data loading. Here we define a batch size of 64, i.e. each element
+# in the dataloader iterable will return a batch of 64 features and labels.
+
+batch_size = 64
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
+
+
+# Creating Models
+
+
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
+
+
+# Optimizing the Model Parameters
+
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+# In a single training loop, the model makes predictions on the training dataset (fed to it in batches), and
+# backpropagates the prediction error to adjust the model's parameters.
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+# We also check the model's performance against the test dataset to ensure it is learning.
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
+
+
+# Saving Models
+# A common way to save a model is to serialize the internal state dictionary (containing the model parameters).
+
+torch.save(model.state_dict(), "Pytorch/models/model.pth")
+print("Saved PyTorch Model State to model.pth")
+
+
+# Loading Models
+
+# The process for loading a model includes re-creating the model structure and loading
+# the state dictionary into it.
+
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("Pytorch/models/model.pth"))
+
+# This model can now be used to make predictions.
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
